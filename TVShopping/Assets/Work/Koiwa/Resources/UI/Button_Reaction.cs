6164ab3c9189156092ea3d16310948c06f266a6f
rewish <spidertargal@yahoo.co.jp>
2016-01-08 22:39:45 +0900

    点数の加算関数を呼んでみた

--- TVShopping/Assets/Work/Koiwa/Resources/UI/Button_Reaction.cs
+++ TVShopping/Assets/Work/Koiwa/Resources/UI/Button_Reaction.cs
@@ -6,6 +6,7 @@ public class Button_Reaction : MonoBehaviour
 {
     public bool isClick = false;
     ScenarioSetter _scenario;
+    ScoreManager _score;
 
     public void OnClick()
     {
@@ -13,16 +14,19 @@ public class Button_Reaction : MonoBehaviour
 
         if (this.name == "Button(0)")
         {
+            _score.ScoreValueUp(Random.Range(1.0f, 15.0f));
             _scenario.SetRoute = ScenarioSetter.Route.A;
         }
 
         if (this.name == "Button(1)")
         {
+            _score.ScoreValueUp(Random.Range(10.0f, 20.0f));
             _scenario.SetRoute = ScenarioSetter.Route.B;
         }
 
         if (this.name == "Button(2)")
         {
+            _score.ScoreValueUp(Random.Range(10.0f, 10.0f));
             _scenario.SetRoute = ScenarioSetter.Route.C;
         }
     }
@@ -30,5 +34,6 @@ public class Button_Reaction : MonoBehaviour
     void Start()
     {
         _scenario = GameObject.FindObjectOfType<ScenarioSetter>();
+        _score = GameObject.FindObjectOfType<ScoreManager>();
     }
 }
